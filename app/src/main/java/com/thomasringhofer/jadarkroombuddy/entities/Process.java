de50b7b58d2f58babcd2eb1a9abb63c621b58ce2
Thomas Ringhofer <t.ringhofer@mjl.at>
2018-02-09 20:51:31 +0100

    CreationDate und Id hinzugefügt.

--- app/src/main/java/com/thomasringhofer/jadarkroombuddy/entities/Process.java
+++ app/src/main/java/com/thomasringhofer/jadarkroombuddy/entities/Process.java
@@ -2,12 +2,24 @@ package com.thomasringhofer.jadarkroombuddy.entities;
 
 import com.google.gson.Gson;
 
+import java.time.LocalDateTime;
+
 /**
  * Representation of a Developmentprocess.
  * Created by Thomas on 09.02.2018.
  */
 
 public class Process implements JsonSerializable<Process> {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     private String title;
 
@@ -19,7 +31,16 @@ public class Process implements JsonSerializable<Process> {
         this.title = title;
     }
 
-    @Override
+    private LocalDateTime creationDate;
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
     public Process deserialize(String jsonValue) {
         Gson gson = new Gson();
         Process instance = gson.fromJson(jsonValue,Process.class);
@@ -31,5 +52,24 @@ public class Process implements JsonSerializable<Process> {
         Gson gson = new Gson();
         String string = gson.toJson(this);
         return string;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        boolean result = true;
+
+        if(obj.getClass().equals(this.getClass()))
+        {
+            Process incoming = (Process)obj;
+            result &= this.getTitle().equals(incoming.getTitle());
+            result &= this.getId().equals(incoming.getId());
+            result &= this.getCreationDate().equals(incoming.getCreationDate());
+
+        }else{
+            return false;
+        }
+
+        return result;
     }
 }
--- app/src/test/java/com/thomasringhofer/jadarkroombuddy/entities/ProcessTest.java
+++ app/src/test/java/com/thomasringhofer/jadarkroombuddy/entities/ProcessTest.java
@@ -1,6 +1,10 @@
 package com.thomasringhofer.jadarkroombuddy.entities;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.time.LocalDateTime;
+
 import static org.junit.Assert.*;
 
 /**
@@ -10,17 +14,30 @@ import static org.junit.Assert.*;
 
 public class ProcessTest {
 
+    private String serializedObject;
+    private Process deserializedObject;
+
+    @Before
+    public void setUp(){
+        serializedObject =  "{\"id\":\"id\",\"title\":\"test\",\"creationDate\":{\"date\":{\"year\":2028,\"month\":1,\"day\":1},\"time\":{\"hour\":20,\"minute\":50,\"second\":0,\"nano\":0}}}";
+        deserializedObject = new Process();
+        deserializedObject.setTitle("test");
+        deserializedObject.setId("id");
+        deserializedObject.setCreationDate(LocalDateTime.of(2028,1,1,20,50));
+    }
+
     @Test
     public void serialization_is_correct() throws Exception{
-        String awaitedString = "{\"title\":\"test\"}";
-        Process process = new Process();
-        process.setTitle("test");
-        String resultString = process.serialize();
+        String awaitedString = serializedObject;
+        String resultString = deserializedObject.serialize();
         assertEquals(awaitedString, resultString);
     }
 
+    @Test
     public void deserialization_is_correct(){
-
+        Process awaitedProcess = deserializedObject;
+        String resultString =serializedObject;
+        Process resultProcess = new Process().deserialize(resultString);
+        assertEquals(awaitedProcess,resultProcess);
     }
-
 }
