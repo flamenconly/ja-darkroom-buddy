258768069649249cb057ee0e11faaa4ed36acf52
Thomas Ringhofer <t.ringhofer@mjl.at>
2018-02-27 21:47:05 +0100

    Tab Navigation implementiert. Icons die die Farbe je nach Selektion ändern implementiert.

---
+++ app/src/main/java/com/thomasringhofer/jadarkroombuddy/MainAppActivity.java
@@ -0,0 +1,93 @@
+package com.thomasringhofer.jadarkroombuddy;
+
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.thomasringhofer.jadarkroombuddy.adapter.MainAppPagerAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainAppActivity extends AppCompatActivity implements RecentDevelopmentProcessesFragment.OnFragmentInteractionListener,WorkingSolutionsFragment.OnFragmentInteractionListener {
+
+    private MainAppPagerAdapter mainAppPagerAdapter;
+
+    @BindView(R.id.main_pager)
+    ViewPager myViewPager;
+
+    @BindView(R.id.main_tab_layout)
+    TabLayout myTabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_app);
+
+        // Initialize Binding
+        ButterKnife.bind(this);
+
+        // Initialize AppPagerAdapter
+        mainAppPagerAdapter = new MainAppPagerAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(mainAppPagerAdapter);
+
+        // Connect TabLayout with Viewpager
+        myTabLayout.setupWithViewPager(myViewPager);
+
+        // Setting Tab Icons
+        myTabLayout.getTabAt(0).setIcon(R.drawable.ic_content_book_2_selected);
+        myTabLayout.getTabAt(1).setIcon(R.drawable.ic_beaker_science);
+
+        myTabLayout.addOnTabSelectedListener(new MyOnTabSelectedListener());
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    class MyOnTabSelectedListener implements TabLayout.OnTabSelectedListener{
+
+        @Override
+        public void onTabSelected(TabLayout.Tab tab) {
+
+            // Change Color
+            switch (tab.getPosition()){
+                case 0:
+                    tab.setIcon(R.drawable.ic_content_book_2_selected);
+                    break;
+                case 1:
+                    tab.setIcon(R.drawable.ic_beaker_science_selected);
+                    break;
+                default:
+            }
+
+        }
+
+        @Override
+        public void onTabUnselected(TabLayout.Tab tab) {
+            // Change Color
+            switch (tab.getPosition()){
+                case 0:
+                    tab.setIcon(R.drawable.ic_content_book_2);
+                    break;
+                case 1:
+                    tab.setIcon(R.drawable.ic_beaker_science);
+                    break;
+                default:
+            }
+        }
+
+        @Override
+        public void onTabReselected(TabLayout.Tab tab) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/thomasringhofer/jadarkroombuddy/adapter/MainAppPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.thomasringhofer.jadarkroombuddy.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.thomasringhofer.jadarkroombuddy.RecentDevelopmentProcessesFragment;
+import com.thomasringhofer.jadarkroombuddy.WorkingSolutionsFragment;
+
+/**
+ * Created by Thomas on 27.02.2018.
+ */
+
+public class MainAppPagerAdapter extends FragmentPagerAdapter {
+
+    static final int NUM_OF_ELEMENTS = 2;
+
+    public MainAppPagerAdapter(FragmentManager fragmentManager){
+        super(fragmentManager);
+    }
+
+    @Override
+    public int getCount() {
+        return NUM_OF_ELEMENTS;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position){
+            case 0:
+                return RecentDevelopmentProcessesFragment.newInstance("Text1","Text2");
+            case 1:
+                return WorkingSolutionsFragment.newInstance("","");
+        }
+        return null;
+    }
+
+}
