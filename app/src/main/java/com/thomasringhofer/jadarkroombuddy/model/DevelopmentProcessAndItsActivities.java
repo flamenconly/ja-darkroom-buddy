40606504004fa446e2c7774a1c60d3ac6ef48c5a
Thomas Ringhofer <t.ringhofer@mjl.at>
2018-03-04 13:16:18 +0100

    Klasse für die Darstellung eine DevelopmentProcess

---
+++ app/src/main/java/com/thomasringhofer/jadarkroombuddy/model/DevelopmentProcessAndItsActivities.java
@@ -0,0 +1,30 @@
+package com.thomasringhofer.jadarkroombuddy.model;
+
+/**
+ * Created by Thomas on 03.03.2018.
+ */
+
+public class DevelopmentProcessAndItsActivities {
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    private String type;
+
+    private String title;
+
+
+}
